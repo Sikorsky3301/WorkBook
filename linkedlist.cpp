6de21ae0133d5b402d245b50a71a00f77c6d9a6d
Rishi Raj <101174357+Sikorsky3301@users.noreply.github.com>
2024-12-13 13:53:02 +0530

    more ll

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -20,6 +20,27 @@ void insertathead(node* &head , int d){
 	head = temp;
 }
 
+void insertattail(node* &tail , int d){
+	node* temp = new node(d);
+	tail -> next = temp ;
+	tail = tail -> next ;
+}
+
+void insertatposition(node* &head , int position , int d ){
+	node* temp = head ;
+	int cnt = 1 ;
+	while(cnt < position - 1){
+		temp = temp -> next ; 
+		cnt++;
+	}
+
+	//creating a node for d(data)
+	node* nodetoinsert = new node(d);
+	nodetoinsert -> next = temp -> next ;
+	temp -> next = nodetoinsert; 
+
+}
+
 void print(node* &head){
 	node* temp = head;
 	while(temp!=NULL){
@@ -41,12 +62,14 @@ int main()
 
     // head pointed to new node initially 
 	node* head = node1;
-
+	node* tail = node1;
 	print(head);
 
 	// inserting at head 
 	insertathead( head , 15);
 	insertathead(head , 29);
+	insertattail(tail , 88);
+	insertatposition(head , 3 , 90);
 	print(head);
 	return 0;
 }
